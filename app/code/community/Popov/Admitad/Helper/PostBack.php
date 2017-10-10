0e216323876c6de752ea4367e7be2361dae8abff
popovsergiy <popow.sergiy@gmail.com>
2017-10-10 11:04:22 +0300

    Cookies names has been changed

--- app/code/community/Popov/Admitad/Helper/PostBack.php
+++ app/code/community/Popov/Admitad/Helper/PostBack.php
@@ -8,9 +8,50 @@
  * @author Popov Sergiy <popow.serhii@gmail.com>
  * @datetime: 07.06.2017 17:48
  */
-class Popov_Admitad_Helper_PostBack extends Popov_Retag_Helper_PostBack
+class Popov_Admitad_Helper_PostBack extends Mage_Core_Helper_Abstract implements Popov_Retag_Helper_PostBackInterface
 {
-    public function send()
+    public function getUrl()
+    {
+        return Mage::getStoreConfig('popov_admitad/settings/postback_url');
+    }
+
+    public function getParams()
+    {
+        $cookie = Mage::getSingleton('core/cookie');
+        $order = Mage::getModel('sales/order')->load(Mage::getSingleton('checkout/session')->getLastOrderId());
+        $items = $order->getAllVisibleItems();
+
+        $post = [];
+        foreach ($items as $key => $item) {
+            $post[] = [
+                'postback_key' => Mage::getStoreConfig('popov_admitad/settings/postback_key'),
+                'campaign_code' => Mage::getStoreConfig('popov_admitad/settings/campaign_code'),
+                'postback' => 1,
+                'action_code' => 1,
+                'uid' => $cookie->get('ADMITAD_UID'),
+                'order_id' => $order->getIncrementId(),
+                'tariff_code' => 1,
+                'price' => $item->getPrice(),
+                'quantity' => (int) $item->getQtyOrdered(),
+                'position_id' => $key + 1,
+                'position_count' => $order->getTotalItemCount(),
+                'product_id' => $item->getProductId(),
+                'payment_type' => 'sale',
+
+                'coupon' => (int) (bool) $order->getCouponCode(),
+                'old_consumer' => $this->hasCustomerPreviousOrders(),
+                'currency_code' => Mage::app()->getStore()->getCurrentCurrencyCode(),
+                'country_code' => $this->getCountryCode()
+            ];
+            if ($customerId = Mage::getSingleton('customer/session')->getCustomer()->getId()) {
+                $post['client_id'] = $customerId;
+            }
+        }
+
+        return $post;
+    }
+
+    public function sendOld()
     {
         $cookie = Mage::getSingleton('core/cookie');
 		if (!$cookie->get('ADMITAD_UID')) {
